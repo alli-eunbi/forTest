78d2683ba247628a97fe460142c7fd3a239fdd55
Alli_Eunbi <allieunbi@gmail.com>
2024-03-03 23:06:42 +0900

    전체 네임스페이스로 pod 감시 방법 및 abort 함수 활용 필요

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ const kc = new k8s.KubeConfig();
 kc.loadFromFile("src/kubeconfig");
 
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+const watch = new k8s.Watch(kc);
 
 const main = async () => {
   try {
@@ -76,4 +77,40 @@ async function eniInfo(nodeName: string): Promise<void> {
   }
 }
 
+const watchEvent = async () => {
+  try {
+    const req = await watch.watch(
+      "/api/v1/namespaces/default/pods",
+      // optional query parameters can go here.
+      {
+        allowWatchBookmarks: true,
+      },
+      // callback is called for each received object.
+      (type, apiObj, watchObj) => {
+        if (type === "ADDED") {
+          console.log("new object:");
+        } else if (type === "MODIFIED") {
+          console.log("changed object:");
+        } else if (type === "DELETED") {
+          console.log("deleted object:");
+        } else if (type === "BOOKMARK") {
+          console.log(`bookmark: ${watchObj.metadata.resourceVersion}`);
+        } else {
+          console.log("unknown type: " + type);
+        }
+        console.log(apiObj);
+      },
+      // done callback is called if the watch terminates normally
+      (err) => console.error(err)
+    );
+    await delay(5000);
+
+    // // watch returns a request object which you can use to abort the watch.
+    // req.abort();
+  } catch (err) {
+    console.error(err);
+  }
+};
 main();
+
+watchEvent();
